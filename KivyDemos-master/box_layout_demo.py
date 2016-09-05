0ea0156b7bc64a3c1856571f59fa78f4285d01cb
jc374321 <Freedom01>
2016-09-05 11:37:38 +1000

    Finish Greet Program

--- KivyDemos-master/box_layout_demo.py
+++ KivyDemos-master/box_layout_demo.py
@@ -8,5 +8,13 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("greet")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def handle_clear(self):
+        print("clear")
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 BoxLayoutDemo().run()
